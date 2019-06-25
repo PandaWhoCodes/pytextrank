a139eddad9a0cb1e55d41ee3dd33fb6fd3e9843a
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2019-06-25 20:47:16 +0530

    Update pytextrank.py

--- pytextrank/pytextrank.py
+++ pytextrank/pytextrank.py
@@ -794,24 +794,9 @@ def top_keywords_sentences(text,stopwords=None, spacy_nlp=None, skip_ner=True, p
     graph, ranks = text_rank(parse_list)
     norm_rank=normalize_key_phrases(parse_list, ranks, stopwords=stopwords, spacy_nlp=spacy_nlp, skip_ner=skip_ner)
     norm_rank_list=[json.loads(pretty_print(rl._asdict())) for rl in norm_rank ]
-    phrases = ", ".join(set([p for p in limit_keyphrases(norm_rank_list, phrase_limit=phrase_limit)]))
+    phrases = set([p for p in limit_keyphrases(norm_rank_list, phrase_limit=phrase_limit)])
 
-    # return a matrix like result for the top keywords
-    kernel = rank_kernel(norm_rank_list)
-
-    # Rank the sentences
-    top_sent=top_sentences(kernel, parse_list)
-    top_sent_list=[json.loads(pretty_print(s._asdict())) for s in top_sent ]
-    sent_iter = sorted(limit_sentences(top_sent_list, word_limit=sent_word_limit), key=lambda x: x[1])
-
-    # Return ranked sentences
-    s=[]
-    for sent_text, idx in sent_iter:
-        s.append(make_sentence(sent_text))
-
-    graf_text = " ".join(s)
-
-    return(graf_text,phrases)
+    return phrases
 
 
 ######################################################################
